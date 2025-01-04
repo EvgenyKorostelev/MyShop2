851aed2b5aa2d6357c0bdaec5c6d9a2e982f3500
Evgeny Korostelev <ddc_smoke@mail.ru>
2025-01-04 09:40:49 +0300

    add_all_productsControllerTests_for_customerApp

--- customer-app/src/test/java/ru/korostelev/customer/controller/ProductsControllerTest.java
+++ customer-app/src/test/java/ru/korostelev/customer/controller/ProductsControllerTest.java
@@ -20,8 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Модульные тесты ProductsController")
@@ -47,7 +46,7 @@ public class ProductsControllerTest {
     void getProductsListPageWithFilter() {
         String filter = "Molotok";
         Product product = new Product(1, "Molotok", "Bolshoy");
-        BasketProduct basketProduct = new BasketProduct(1,1);
+        BasketProduct basketProduct = new BasketProduct(1, 1);
         List<BasketProduct> basketProducts = List.of(basketProduct);
         List<Product> filteredProducts = List.of(product);
         when(productsClient.findAllProducts(filter)).thenReturn(filteredProducts);
@@ -85,13 +84,105 @@ public class ProductsControllerTest {
     }
 
     @Test
-    @DisplayName("getFavouriteProductsPage вернет страницу отфильтрованых избраных товаров ")
-    void getFavouriteProductsPageWithFilter(){
+    @DisplayName("getFavouriteProductsPage вернет страницу отфильтрованных избранных товаров")
+    void getFavouriteProductsPageWithFilter() {
         String filter = "Molotok";
-        Product product = new Product(1, "Molotok", "Bolshoy");
-        BasketProduct basketProduct = new BasketProduct(1,1);
-        FavouriteProduct favouriteProduct = new FavouriteProduct(UUID.randomUUID(), product.id());
-        List<FavouriteProduct> favouriteProductsList = List.of(favouriteProduct);
+        List<FavouriteProduct> favouriteProducts = List.of(
+                new FavouriteProduct(UUID.randomUUID(), 1),
+                new FavouriteProduct(UUID.randomUUID(), 3));
+        List<Product> filteredProducts = List.of(
+                new Product(1, "Molotok", "Bolshoy"));
+        List<BasketProduct> basketProducts = List.of(new BasketProduct(1, 1));
+        when(favouriteProductsClient.findFavouriteProducts()).thenReturn(favouriteProducts);
+        when(productsClient.findAllProducts(filter)).thenReturn(filteredProducts);
+        when(basketClient.findProductsInBasket()).thenReturn(basketProducts);
 
+        String viewName = productsController.getFavouriteProductsPage(model, filter);
+
+        assertEquals("customer/products/favourites", viewName);
+        verify(model).addAttribute("filter", filter);
+        verify(model).addAttribute("products", filteredProducts);
+        verify(model).addAttribute("basket", 1);
+        verify(favouriteProductsClient).findFavouriteProducts();
+        verify(productsClient, atLeastOnce()).findAllProducts(filter);
+        verify(basketClient).findProductsInBasket();
+    }
+
+    @Test
+    @DisplayName("getFavouriteProductsPage вернет страницу избранных товаров без фильтра")
+    void getFavouriteProductsPageWithoutFilter() {
+        String filter = null;
+        List<FavouriteProduct> favouriteProducts = List.of(
+                new FavouriteProduct(UUID.randomUUID(), 1),
+                new FavouriteProduct(UUID.randomUUID(), 3));
+        List<Product> allProducts = List.of(
+                new Product(1, "Molotok", "Bolshoy"),
+                new Product(2, "Myach", "Krugliy"),
+                new Product(3, "Topor", "Ostriy"));
+        List<Product> foundFavouriteProducts = List.of(
+                new Product(1, "Molotok", "Bolshoy"),
+                new Product(3, "Topor", "Ostriy"));
+        when(favouriteProductsClient.findFavouriteProducts()).thenReturn(favouriteProducts);
+        when(productsClient.findAllProducts(filter)).thenReturn(allProducts);
+        when(basketClient.findProductsInBasket()).thenReturn(List.of());
+
+        String viewName = productsController.getFavouriteProductsPage(model, filter);
+
+        assertEquals("customer/products/favourites", viewName);
+        verify(model).addAttribute("filter", filter);
+        verify(model).addAttribute("products", foundFavouriteProducts);
+        verify(model).addAttribute("basket", 0);
+        verify(favouriteProductsClient).findFavouriteProducts();
+        verify(productsClient, atLeastOnce()).findAllProducts(filter);
+        verify(basketClient).findProductsInBasket();
+    }
+
+
+    @Test
+    @DisplayName("getBasketPage вернет страницу отфильтрованных товаров в корзине")
+    void getBasketPageWithFilter() {
+        String filter = "Molotok";
+        List<BasketProduct> basketProducts = List.of(
+                new BasketProduct(1, 1),
+                new BasketProduct(2, 5));
+        List<Product> filteredProducts = List.of(
+                new Product(1, "Molotok", "Bolshoy"));
+        when(basketClient.findProductsInBasket()).thenReturn(basketProducts);
+        when(productsClient.findAllProducts(filter)).thenReturn(filteredProducts);
+
+        String viewName = productsController.getBasketPage(model, filter);
+
+        assertEquals("customer/products/basket", viewName);
+        verify(model).addAttribute("filter", filter);
+        verify(model).addAttribute("products", filteredProducts);
+        verify(basketClient).findProductsInBasket();
+        verify(productsClient, atLeastOnce()).findAllProducts(filter);
+    }
+
+    @Test
+    @DisplayName("getBasketPage вернет страницу товаров в корзине без фильтра")
+    void getBasketPageWithoutFilter() {
+        String filter = null;
+        List<BasketProduct> basketProducts = List.of(
+                new BasketProduct(1, 1),
+                new BasketProduct(2, 5));
+        List<Product> allProducts = List.of(
+                new Product(1, "Molotok", "Bolshoy"),
+                new Product(2, "Myach", "Krugliy"),
+                new Product(3, "Topor", "Ostriy"));
+        List<Product> foundBasketProducts = List.of(
+                new Product(1, "Molotok", "Bolshoy"),
+                new Product(2, "Myach", "Krugliy"));
+        when(basketClient.findProductsInBasket()).thenReturn(basketProducts);
+        when(productsClient.findAllProducts(filter)).thenReturn(allProducts);
+
+        String viewName = productsController.getBasketPage(model, filter);
+
+        assertEquals("customer/products/basket", viewName);
+        verify(model).addAttribute("filter", filter);
+        verify(model).addAttribute("products", foundBasketProducts);
+        verify(basketClient).findProductsInBasket();
+        verify(productsClient, atLeastOnce()).findAllProducts(filter);
     }
 }
+
